10eb024ae578c098e98d5dbff39344b4fb700349
viniciusestumano <vsestumano@gmail.com>
2023-05-28 20:06:06 -0300

    css com js e modal adicionados

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,28 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+const modalControler = document.getElementById('modal__controler')
+const modalOpen = document.getElementById('button-modal_open')
+const modalClose = document.getElementById('button-modal_close')
+
+function closeModal () {
+
+    modalControler.style.display = 'none';
+
+}
+
+function openModal () {
+
+    modalControler.style.display = 'flex';
+    
+}
+
+function closeModalBody(event) {
+    console.log('seeeeeeeee');
+    if(event.target == modalControler){
+        closeModal()
+    }
+}
+
+window.addEventListener('click', closeModalBody)
+modalOpen.addEventListener('click', openModal)
+modalClose.addEventListener('click', closeModal)
+
